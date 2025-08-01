97953f06705036324bbcbcebcc5071252c730c71
CHOI  SEONGUN <apple000330@naver.com>
2025-08-01 09:12:16 +0900

    (choi)一次元輝度分布センサのGUI

--- web-obrid-monitor-app/app/grap/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Grap from "@/components/Grap";
-
-export default function Home() {
-  return <Grap />;
-}
--- web-obrid-monitor-app/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { EventDisplay } from "@/components/EventDisplay";
-
-export default function Home() {
-  return <EventDisplay />;
-}
